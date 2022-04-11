1b6ad5a593e21f1e49d3161a149318d4cd5bf697
Jerry Castro <jerry.castro@hp.com>
2022-04-11 16:28:44 -0300

    Add project files.

---
+++ src/NerdStore.Catalog.Domain/Product.cs
@@ -0,0 +1,73 @@
+﻿using NerdStore.Core.DomainObjects;
+using System;
+
+namespace NerdStore.Catalog.Domain
+{
+    public class Product : Entity, IAggregateRoot
+    {
+        public Guid CategoryId { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public bool Active { get; private set; }
+        public decimal Value { get; private set; }
+        public DateTime RegisterDate { get; private set; }
+        public string Image { get; private set; }
+        public int InventoryQuantity { get; private set; }
+        public Category Category { get; private set; }
+
+        public Product(string name, string description, bool active, decimal value, Guid categoryId, DateTime registerDate, string image)
+        {
+            CategoryId = categoryId;
+            Name = name;
+            Description = description;
+            Active = active;
+            Value = value;
+            RegisterDate = registerDate;
+            Image = image;
+        }
+
+        public void Activate() => Active = true;
+        public void Deactivate() => Active = false;
+        public void ChangeCategory(Category category)
+        {
+            Category = category;
+            CategoryId = category.Id;
+        }
+        public void ChangeDescription(string description)
+        {
+            Description = description; 
+        }
+
+        public void DecreaseIventory(int quantity)
+        {
+            if (quantity < 0) quantity *= -1;
+            InventoryQuantity -= quantity;
+        }
+        public void IncreaseInventory(int quantity)
+        {
+            InventoryQuantity += quantity;
+        } 
+        public bool HasInventory(int quantity)
+        {
+            return InventoryQuantity >= quantity;
+        }
+        public void Validate() 
+        { 
+            
+
+        
+        }
+
+    }
+    public class Category : Entity
+    {
+        public string Name { get; private set; }
+        public string Code { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Code}";
+        }
+    }
+
+}
---
+++ src/NerdStore.Core/DomainObjects/AssertionConcern.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NerdStore.Core.DomainObjects
+{
+    public class AssertionConcern
+    {
+        public static void ValidateEquals(object obj1, object obj2, string message)
+        {
+            if(!obj1.Equals(obj2))
+            {
+                throw new DomainException(message);
+            }
+        }
+        public static void ValidadeNotEquals(object obj1, object obj2, string message)
+        {
+            if (obj1.Equals(obj2))
+            {
+                throw new DomainException(message);
+            }
+        }
+        public static void ValidateCharacters(string value, int max, string message)
+        {
+            var length = value.Trim().Length;
+            if (length > max)
+            {
+                throw new DomainException(message);
+            }
+        }
+        public static void ValidateCharacters(string value,int min, int max, string message)
+        {
+            var length = value.Trim().Length;
+            if (length < min || length > max)
+            {
+                throw new DomainException(message);
+            }
+        }
+        public static void ValidadeExpression(string pattern, string value, string message) 
+        {
+            var regex = new Regex(pattern);
+            if(!regex.IsMatch(value))
+            {
+                throw new DomainException(message);
+            }
+        }
+        public static void ValidadeEmpty(string value, string message)
+        {
+            if(value == null || value.Trim().Length == 0)
+            {
+                throw new DomainException(message);
+            }
+        }
+    }
+}
